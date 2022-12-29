a9a098af2f578c394e5da7b2ba3b73dce08c055a
bekir <bekir.akbasoglu@gmail.com>
2022-12-29 21:19:50 +0300

    commit for dev

---
+++ server.js
@@ -0,0 +1,15 @@
+// Constants
+const express = require('express');
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+
+// App
+const app = express();
+app.get('/', (req, res) => {res.send('Hello World Everyone!!');});
+app.get('/will', (req, res) => {res.send('Hello World Asisteer !!');});
+app.get('/ready', (req, res) => {res.send('Hello World Ready!');});
+
+app.listen(PORT, HOST, () => {console.log('Running on http://${HOST}:${PORT}');});
+
+
