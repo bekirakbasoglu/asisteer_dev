6c8218872dbe314c16b33de35b443ef8df1ccf82
bekir <bekir.akbasoglu@gmail.com>
2022-12-31 01:12:58 +0300

    commit for dev new

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/will', (req, res) => {res.send('Hello World Asisteer !!');});
 app.get('/ready', (req, res) => {res.send('Hello World Ready!');});
 app.get('/bekir', (req, res) => {res.send('Hello World bekir!');});
 app.get('/ali', (req, res) => {res.send('Hello World ali!');});
+app.get('/resim', (req, res) => {res.send('Hello World resim!');});
 
 app.listen(PORT, HOST, () => {console.log('Running on http://${HOST}:${PORT}');});
 
