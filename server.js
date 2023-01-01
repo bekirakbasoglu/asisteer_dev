b5595de15e7328a7228ff0d92d47d86507a1d85c
bekir <bekir.akbasoglu@gmail.com>
2023-01-01 22:05:39 +0300

    commit for dev dimanche

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/bekir', (req, res) => {res.send('Hello World bekir!');});
 app.get('/ali', (req, res) => {res.send('Hello World ali!');});
 app.get('/resim', (req, res) => {res.send('Hello World resim!');});
 
-app.listen(PORT, HOST, () => {console.log('Running on http://${HOST}:${PORT}');});
+app.listen(PORT, HOST, () => {console.log(`Running on http://${HOST}:${PORT}`);});
 
 
